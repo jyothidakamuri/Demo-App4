bd9fd00f5e41d638ece0086a0c4f46844ea76ee8
Jyothi <jyothi.dakamuri@gmail.com>
2023-12-03 22:50:17 +0530

    project initial commit

---
+++ src/main/java/com/example/GITDemo4/GitDemo4Application.java
@@ -0,0 +1,13 @@
+package com.example.GITDemo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GITDemo4/GitDemo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GITDemo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
