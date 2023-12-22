91b66fef5f5605515edc275cebccb80800f764d0
Jyothi <jyothi.dakamuri@gmail.com>
2023-12-22 15:49:17 +0530

    Main class modified

--- src/main/java/com/example/GITDemo4/GitDemo4Application.java
+++ src/main/java/com/example/GITDemo4/GitDemo4Application.java
@@ -2,12 +2,17 @@ package com.example.GITDemo4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
 public class GitDemo4Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GitDemo4Application.class, args);
+
+		ConfigurableApplicationContext run = SpringApplication.run(GitDemo4Application.class, args);
+
+		run.close();
 	}
 
 }
